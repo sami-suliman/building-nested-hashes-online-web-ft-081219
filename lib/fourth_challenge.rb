132f386686fe3fed640d20d01839c002318407f3
sami suliman <sami.bco@gmail.com>
2019-08-19 23:51:45 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,8 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        { name: "Mercutio", age: "18", attitude: "hot-headed" },
-        { name: "Nurse", age: "44", attitude: "worried" }
+        { name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
